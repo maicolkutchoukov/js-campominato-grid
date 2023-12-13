45e8185d8968dc8324b00c6969e873ca46290599
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-13 16:04:25 +0100

    Completato bonus

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,58 @@
 */
 const myContainer = document.querySelector('.my-container');
 const startButton = document.querySelector('button');
+const easy = document.getElementById('easy');
+const medium = document.getElementById('medium');
+const hard = document.getElementById('hard');
 startButton.addEventListener ('click', function(){
-    
+    myContainer.innerHTML = ""
+    if (easy.selected){
+        const easyContainer = document.createElement('div')
+        easyContainer.classList.add('easy-container');
+        myContainer.append(easyContainer);
     for (let i = 0; i < 100; i++) {
+        // const easyContainer = document.createElement('div')
+        // easyContainer.classList.add('easy-game');
+        
         const square = document.createElement('div');
-        square.classList.add('cell');
+        square.classList.add('cell', 'easy-game');
         square.innerHTML += i + 1;
-        myContainer.append(square);
+        easyContainer.append(square);
         square.addEventListener('click', function(){
             square.classList.toggle('active');
             console.log(square.innerHTML)
         })        
+    }
+    }
+    else if (medium.selected){
+        const mediumContainer = document.createElement('div')
+        mediumContainer.classList.add('medium-container');
+        myContainer.append(mediumContainer);
+        for (let i = 0; i < 81; i++) {
+            const square = document.createElement('div');
+            square.classList.add('cell', 'medium-game');
+            square.innerHTML += i + 1;
+            mediumContainer.append(square);
+            square.addEventListener('click', function(){
+                square.classList.toggle('active');
+                console.log(square.innerHTML)
+            })        
+        }
+    }
+    else if (hard.selected) {
+        const hardContainer = document.createElement('div')
+        hardContainer.classList.add('hard-container');
+        myContainer.append(hardContainer);
+
+        for (let i = 0; i < 49; i++) {
+            const square = document.createElement('div');
+            square.classList.add('cell', 'hard-game');
+            square.innerHTML += i + 1;
+            hardContainer.append(square);
+            square.addEventListener('click', function(){
+                square.classList.toggle('active');
+                console.log(square.innerHTML)
+            })        
+        }
     }
 })
