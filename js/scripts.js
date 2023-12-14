93ad78b26bd91f8c38b795908ed73b85618564e1
maicolkutchoukov <maicol.j.95@gmail.com>
2023-12-14 11:12:35 +0100

    Ottimizzato codice togliendo ripetizioni e if

--- js/scripts.js
+++ js/scripts.js
@@ -9,57 +9,24 @@
     -- emetto un messaggio in console con il numero della cella cliccata.
 
 */
+
+
 const myContainer = document.querySelector('.my-container');
 const startButton = document.querySelector('button');
-const easy = document.getElementById('easy');
-const medium = document.getElementById('medium');
-const hard = document.getElementById('hard');
+const mySelect = document.querySelector('select');
 startButton.addEventListener ('click', function(){
-    myContainer.innerHTML = ""
-    if (hard.selected){
-        const hardContainer = document.createElement('div')
-        hardContainer.classList.add('hard-container');
-        myContainer.append(hardContainer);
-    for (let i = 0; i < 100; i++) {        
+    myContainer.innerHTML = "";
+    const difficult = parseInt(mySelect.value);
+    console.log(difficult);
+    for (let i = 0; i < difficult; i++) {        
         const square = document.createElement('div');
-        square.classList.add('cell', 'hard-game');
+        square.classList.add('cell', 'cell-' + difficult);
         square.innerHTML += i + 1;
-        hardContainer.append(square);
+        myContainer.append(square);
         square.addEventListener('click', function(){
-            square.classList.toggle('active');
+            square.classList.add('active');
             console.log(square.innerHTML)
         })        
-    }
-    }
-    else if (medium.selected){
-        const mediumContainer = document.createElement('div')
-        mediumContainer.classList.add('medium-container');
-        myContainer.append(mediumContainer);
-        for (let i = 0; i < 81; i++) {
-            const square = document.createElement('div');
-            square.classList.add('cell', 'medium-game');
-            square.innerHTML += i + 1;
-            mediumContainer.append(square);
-            square.addEventListener('click', function(){
-                square.classList.toggle('active');
-                console.log(square.innerHTML)
-            })        
-        }
-    }
-    else if (easy.selected) {
-        const easyContainer = document.createElement('div')
-        easyContainer.classList.add('easy-container');
-        myContainer.append(easyContainer);
-
-        for (let i = 0; i < 49; i++) {
-            const square = document.createElement('div');
-            square.classList.add('cell', 'easy-game');
-            square.innerHTML += i + 1;
-            easyContainer.append(square);
-            square.addEventListener('click', function(){
-                square.classList.toggle('active');
-                console.log(square.innerHTML)
-            })        
-        }
-    }
+    }    
 })
+
